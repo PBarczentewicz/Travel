a867cc8ca55071565a5a52298607ecedd20a184e
Przemek <p.barczentewicz@gmail.com>
2024-10-29 11:12:53 +0100

    @Parameterized

---
+++ src/main/java/org/example/flightDataAccess/service/AssertionService.java
@@ -0,0 +1,7 @@
+package org.example.flightDataAccess.service;
+
+import org.example.flightDataAccess.schema.CheapestTicketResponse;
+
+public class AssertionService {
+
+}
--- src/main/java/org/example/flightDataAccess/test/CheapestTicketTest.java
+++ src/main/java/org/example/flightDataAccess/test/CheapestTicketTest.java
@@ -1,20 +1,38 @@
 package org.example.flightDataAccess.test;
 
 import org.example.flightDataAccess.schema.CheapestTicketRequest;
+import org.example.flightDataAccess.service.AssertionService;
 import org.example.flightDataAccess.service.FlightService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import sun.jvm.hotspot.utilities.Assert;
 
 public class CheapestTicketTest {
 
-    @Test
-    public void cheapestTicket() {
+    @ParameterizedTest
+    @CsvSource({
+            "KRK, PLN, 1, WAW, true",
+            "WAW, PLN, 1, KRK, true",
+            "KRK, PLN, 1, GDA, bad request: airport GDA: not flightable",
+    })
+
+    public void cheapestTicket(String destination, String currency, String page, String origin, String textFromWeb) {
 
         CheapestTicketRequest request = CheapestTicketRequest.builder()
-                .destination("KRK")
-                .currency("PLN")
-                .page("1")
-                .origin("WAW").build();
+                .destination(destination)
+                .currency(currency)
+                .page(page)
+                .origin(origin)
+                .build();
 
         new FlightService().GetChepestTicket(request);
+
+        switch (textFromWeb) {
+            case "ok":
+
+
+        }
     }
 }
